28248a6b474b3a694d56d857ec2c46aac2409ece
Vicente Romero <73198039+elbug-py@users.noreply.github.com>
2022-10-06 20:08:23 -0400

    final version

--- parenthesis/test/check_tags.cpp
+++ parenthesis/test/check_tags.cpp
@@ -9,45 +9,65 @@
 using namespace eda;
 
 bool validateParenthesis(const std::string &input, int* pos){
+	std::ofstream log;
+	log.open("output.log");
 	bool error = false;
 	int i = 0;
+	int linea = 1;
+	int posicion=0;
 	Stack stack;
 	while (!error && i < input.length()){
+		if (input[i] == '\n')
+		{
+			linea++;
+			posicion = 0;
+		}
+		
 		if (input[i] == '<') {
 			std::string tag = "<";
 			while (input[i] != '>' && i < input.length())
 			{	
 				i++;
-				
+				posicion++;
 				
 				tag += input[i];
+				
 				
 			}
 			;
 			if (tag[1] == '/')
-				{
+				{	
+					std::string tag_og = tag;
 					tag.erase(1,1); //borra el / del tag
 					if (!stack.isEmpty() && tag.compare(stack.top()->getData())== 0 )
 					{
 						stack.pop();
+						log<<"tag "<<tag<<" ok"<<std::endl;
 					}
 					else{
-						*pos = i;
+
+						log<<"error en la linea "<<linea<<" posicion "<<posicion<<" el tag de cierre "<<tag_og<<" no corresponde al último tag abierto"<<std::endl;
 						return error;
 					}
 					
 				}
-			else{
+			else{	
+					
 					stack.push(new Node(tag));
 				}
-			std::cout<<tag<<std::endl;
+			
 		}
 		i = i + 1;
 	}
 	if (!stack.isEmpty()){
+		
 		error = true;
 	}
+	else{
+		log<<"0 errores, archivo válido";
+	}
 	*pos = i;
+	log.close();
 	return !error;
 }
 
@@ -62,13 +82,7 @@ int main(int nargs, char** vargs){
 		std::string input = stream.str();
 		int pos=0;
 		bool status = validateParenthesis(input, &pos);
-		if (status){
-			std::cout<< " Expresión Correcta " << std::endl;
-		}
-		else{
-			std::cout<< " Expresión Inválida" << std::endl;
-			std::cout<< "Pos error:  " << pos - 1	<< std::endl;
-		}
+		
 	}
 	else{
 		std::cout<<"Por favor ingresar un archivo html"<<std::endl;
